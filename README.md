08465d08990a28a17b1595356c1c370586f77a98
qiwan <wanqicn@qq.com>
2023-08-22 17:09:58 +0800

    feat: support underlines and bolded text

--- README.md
+++ README.md
@@ -1,12 +1,46 @@
-# Hidelight - Obsidian Plugin
+# Cloze - Obsidian Plugin
 
-Inspired from anki cards, this simple plugin for [Obsidian](https://obsidian.md/) allows you to toggle the visibility🙈 of highlighted text (only under reading mode) with only one click, which helps effortlessly internalize and retain crucial information.
+Inspired from anki cards, this simple plugin for [Obsidian](https://obsidian.md/) enables you to create a cloze from highlighted, underlined, or bolded texts, as well as any selected text in reading mode.
 
-Enjoy giving yourself a memorizing exercise at any time!
+![Cloze Plugin](https://raw.githubusercontent.com/dearvikki/obsidian-cloze-plugin/main/assets/demo.gif)
 
 ## Usage
 
-Simply click on the highlighted area to toggle its visibility. If you want to toggle the visibility of all highlighted texts, just click on the ribbon icon. Additionally, you have the option to hide all highlighted texts by default in the settings.
+### Create clozes
+
+There're certain ways for creating clozes.
+
+1. Convert from specific text segments
+
+By enabling the following three settings, the corresponding text will automatically be converted into clozes in reading mode.
+
+![Cloze Plugin](https://raw.githubusercontent.com/dearvikki/obsidian-cloze-plugin/main/assets/settings1.png)
+
+![Cloze Plugin](https://raw.githubusercontent.com/dearvikki/obsidian-cloze-plugin/main/assets/enable_highlight.gif)
+
+2. Custom clozes
+
+By default, the plugin adds 2 menu items: "Add Cloze" and "Remove Cloze", which are only visible when text is selected. The selected text will be underlined to indicate it has been clozed.
+
+![Cloze Plugin](https://raw.githubusercontent.com/dearvikki/obsidian-cloze-plugin/main/assets/add.png)
+
+![Cloze Plugin](https://raw.githubusercontent.com/dearvikki/obsidian-cloze-plugin/main/assets/remove.png)
+
+Note: When clozing text, a custom `<span></span>` will be added around the text. Therefore, if you intend to export the file, please ensure that all custom clozes have been removed. You can conveniently select and remove multiple clozed texts at once.
+
+### Utilizing clozes
+
+Note: it only works in reading mode. Everything remains same in editing mode.
+
+To toggle the visibility of a cloze area, simply click on it. 
+
+If you want to toggle the visibility of all clozes, click on the ribbon icon --- a small fish.
+
+![Cloze Plugin](https://raw.githubusercontent.com/dearvikki/obsidian-cloze-plugin/main/assets/fish.png)
+
+![Cloze Plugin](https://raw.githubusercontent.com/dearvikki/obsidian-cloze-plugin/main/assets/fish-mobile.png)
+
+Additionally, all clozes will be hidden by default, but you can disable this feature in the settings.
 
 ## Installation
 
--- main.ts
+++ main.ts
@@ -1,13 +1,19 @@
-import { App, Plugin, PluginSettingTab, Setting, } from 'obsidian';
+import { App, Plugin, PluginSettingTab, Setting, Menu, Editor } from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
 interface MyPluginSettings {
 	defaultHide: boolean;
+	includeHighlighted: boolean;
+	includeUnderlined: boolean;
+	includeBolded: boolean;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	defaultHide: false
+	defaultHide: true,
+	includeHighlighted: false,
+	includeUnderlined: false,
+	includeBolded: false,
 }
 
 export default class MyPlugin extends Plugin {
@@ -16,42 +22,74 @@ export default class MyPlugin extends Plugin {
 	isAllHide: boolean = false;
 
 	async onload() {
-		console.log('load hidelight plugin')
+		console.log('load cloze plugin');
+
 		await this.loadSettings();
 
 		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('replace', 'toggle highlighted text', (evt: MouseEvent) => {
+		this.addRibbonIcon('fish', 'Toggle cloze', (evt: MouseEvent) => {
 			// Called when the user clicks the icon.
 			this.toggleAllHide(!this.isAllHide);
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-		
+		});		
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-
 		this.registerDomEvent(document, 'click', (event) => {
-			const target = event.target as HTMLElement;
-			if (target.matches('.markdown-reading-view mark')) {
-			  // Perform the desired action when the specific HTML tag is clicked
-			  // You can modify the DOM, trigger other functions, or interact with Obsidian's API here
-				if(target.getAttribute('data-mark-hide')) {
-					target.style.color = 'inherit';
-					target.removeAttribute('data-mark-hide');
-				} else {
-					target.style.color = 'transparent';
-					target.setAttribute('data-mark-hide', 'true');
-				}
-			}
+			this.toggleHide(event.target as HTMLElement);
 		});
 	
-		this.app.workspace.on('file-open', async () => {
-			this.toggleAllHide(this.settings.defaultHide);
-		});
+		this.registerEvent(
+			this.app.workspace.on('file-open', async () => {
+				setTimeout(() => {
+					this.addClass();
+					this.toggleAllHide(this.settings.defaultHide);
+				}, 0);
+			})
+		);
+
+		this.registerEvent(
+			this.app.workspace.on("editor-menu", (
+				menu: Menu,
+				editor: Editor
+			  ): void => {
+				const selection = editor.getSelection();
+				if (selection) {
+					menu.addItem((item) => {
+						item
+						  .setTitle("Add cloze")
+						  .onClick((e) => {
+							  this.addCloze(editor);
+						  });
+					});
+					menu.addItem((item) => {
+					  item
+						.setTitle("Remove cloze")
+						.onClick((e) => {
+							this.removeCloze(editor);
+						});
+					});
+				  }
+			  })
+		);
+
+		this.addCommand({
+			id: "add-cloze",
+			name: "Turn into cloze",
+			icon: "fish",
+			editorCallback: (editor, context) => {
+				this.addCloze(editor);
+			}
+		  })
+
+		this.addCommand({
+			id: "remove-cloze",
+			name: "Remove cloze",
+			icon: "fish-off",
+			editorCallback: async (editor: Editor) => {
+				this.removeCloze(editor);
+			},
+		})
 	}
 
 	onunload() {
@@ -66,22 +104,85 @@ export default class MyPlugin extends Plugin {
 		await this.saveData(this.settings);
 	}
 
+	addClass() {
+		const markdownView = document.querySelector('.markdown-reading-view');
+		if (markdownView) {
+			markdownView.classList.remove('cloze-highlighted', 'cloze-underlined', 'cloze-bolded');
+			if (this.settings.includeHighlighted) {
+				markdownView.classList.add('cloze-highlighted');
+			}
+			if (this.settings.includeUnderlined) {
+				markdownView.classList.add('cloze-underlined');
+			}
+			if (this.settings.includeBolded) {
+				markdownView.classList.add('cloze-bolded');
+			}
+		}
+	}
+
+	clozeSelector = () => {
+		const selectors = ['.markdown-reading-view .cloze-span'];
+		if (this.settings.includeHighlighted) {
+			selectors.push('.markdown-reading-view.cloze-highlighted mark');
+		}
+		if (this.settings.includeUnderlined) {
+			selectors.push('.markdown-reading-view.cloze-underlined u');
+		}
+		if (this.settings.includeBolded) {
+			selectors.push('.markdown-reading-view.cloze-bolded strong');
+		}
+		return selectors.join(', ');
+	}
+
+	hideClozeContent = (target: HTMLElement) => {
+		target.style.color = 'transparent';
+		target.setAttribute('data-mark-hide', 'true');
+	}
+
+	showClozeContent = (target: HTMLElement) => {
+		target.style.color = 'inherit';
+		target.removeAttribute('data-mark-hide');
+	}
+
+	toggleHide(target: HTMLElement) {
+		if (target.matches(this.clozeSelector())) {
+			if(target.getAttribute('data-mark-hide')) {
+				this.showClozeContent(target);
+			} else {
+				this.hideClozeContent(target);
+			}
+		}
+	}
+
 	toggleAllHide(hide: boolean) {
-		const marks = document.querySelectorAll<HTMLElement>('.markdown-reading-view mark');
+		const marks = document.querySelectorAll<HTMLElement>(this.clozeSelector());
 		if(hide) {
 			marks.forEach((mark) => {
-				mark.style.color = 'transparent';
-				mark.setAttribute('data-mark-hide', 'true');
+				this.hideClozeContent(mark);
 			})
 			this.isAllHide = true;
 		} else {
 			marks.forEach((mark) => {
-				mark.style.color = 'inherit';
-				mark.removeAttribute('data-mark-hide');
+				this.showClozeContent(mark);
 			})
 			this.isAllHide = false;
 		}
 	}
+
+	addCloze = (editor: Editor) => {
+		const currentStr = editor.getSelection();
+		const newStr = currentStr
+		  		.replace(/\<span class="cloze-span">(.*?)<\/span>/g, "$1");
+		editor.replaceSelection(`<span class="cloze-span">${newStr}</span>`);
+		editor.blur();
+	}
+
+	removeCloze = (editor: Editor) => {
+		const currentStr = editor.getSelection();
+		const newStr = currentStr
+		  .replace(/\<span class="cloze-span">(.*?)<\/span>/g, "$1");
+		editor.replaceSelection(newStr);
+	  };
 }
 
 class SettingTab extends PluginSettingTab {
@@ -96,7 +197,7 @@ class SettingTab extends PluginSettingTab {
 		const {containerEl} = this;
 
 		containerEl.empty();
-		containerEl.createEl('h1', {text: 'Hidelight'});
+		containerEl.createEl('h1', {text: 'Cloze'});
     	containerEl.createEl("p", { text: "Created by " }).createEl("a", {
 			text: "Vikki",
 			href: "https://github.com/DearVikki",
@@ -105,8 +206,38 @@ class SettingTab extends PluginSettingTab {
     	containerEl.createEl('h2', {text: 'Settings'})
 
 		new Setting(containerEl)
+			.setName('Convert highlights into clozes')
+			.setDesc('Enabling this setting will convert all ==highlights== into clozes as well. ')
+			.addToggle(toggle => toggle
+				.setValue(this.plugin.settings.includeHighlighted)
+				.onChange(value => {
+					this.plugin.settings.includeHighlighted = value;
+					this.plugin.saveSettings();
+				}))
+
+		new Setting(containerEl)
+			.setName('Convert bolded text into clozes')
+			.setDesc('Enabling this setting will convert all **bolded text** into clozes as well. ')
+			.addToggle(toggle => toggle
+				.setValue(this.plugin.settings.includeBolded)
+				.onChange(value => {
+					this.plugin.settings.includeBolded = value;
+					this.plugin.saveSettings();
+				}))
+
+		new Setting(containerEl)
+			.setName('Convert underlines into clozes')
+			.setDesc('Enabling this setting will convert all <u>underlines</u> into clozes as well. ')
+			.addToggle(toggle => toggle
+				.setValue(this.plugin.settings.includeUnderlined)
+				.onChange(value => {
+					this.plugin.settings.includeUnderlined = value;
+					this.plugin.saveSettings();
+				}))
+
+		new Setting(containerEl)
 			.setName('Hide by default')
-			.setDesc('By enabling this setting, all highlighted texts will be hidden by default. 🙈 ')
+			.setDesc('By enabling this setting, all cloze content on the page will be hidden by default when it is opened. 🙈 ')
 			.addToggle(toggle => toggle
 				.setValue(this.plugin.settings.defaultHide)
 				.onChange(value => {
